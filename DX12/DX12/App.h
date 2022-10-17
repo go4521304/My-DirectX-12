db2d7d9a0b76481614da71157c111a6cb7fdd5f3
Kclient <go4521304@gmail.com>
2022-10-18 02:18:43 +0900

    Remove project and Make Sample project

---
+++ DX12/DX12/App.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "pch.h"
+#include "Common\DeviceResources.h"
+#include "DX12Main.h"
+
+namespace DX12
+{
+	// 애플리케이션의 주 진입점입니다. 애플리케이션을 Windows 셸과 연결하고 애플리케이션 수명 주기 이벤트를 처리합니다.
+	ref class App sealed : public Windows::ApplicationModel::Core::IFrameworkView
+	{
+	public:
+		App();
+
+		// IFrameworkView 메서드.
+		virtual void Initialize(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView);
+		virtual void SetWindow(Windows::UI::Core::CoreWindow^ window);
+		virtual void Load(Platform::String^ entryPoint);
+		virtual void Run();
+		virtual void Uninitialize();
+
+	protected:
+		// 애플리케이션 수명 주기 이벤트 처리기입니다.
+		void OnActivated(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView, Windows::ApplicationModel::Activation::IActivatedEventArgs^ args);
+		void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ args);
+		void OnResuming(Platform::Object^ sender, Platform::Object^ args);
+
+		// 창 이벤트 처리기입니다.
+		void OnWindowSizeChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::WindowSizeChangedEventArgs^ args);
+		void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
+		void OnWindowClosed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CoreWindowEventArgs^ args);
+
+		// DisplayInformation 이벤트 처리기입니다.
+		void OnDpiChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnOrientationChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnDisplayContentsInvalidated(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+
+	private:
+		// m_deviceResources에 대한 프라이빗 접근자는 디바이스 제거 오류로부터 보호합니다.
+		std::shared_ptr<DX::DeviceResources> GetDeviceResources();
+
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+		std::unique_ptr<DX12Main> m_main;
+		bool m_windowClosed;
+		bool m_windowVisible;
+	};
+}
+
+ref class Direct3DApplicationSource sealed : Windows::ApplicationModel::Core::IFrameworkViewSource
+{
+public:
+	virtual Windows::ApplicationModel::Core::IFrameworkView^ CreateView();
+};
